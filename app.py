746ef550ab818e186790e73aa53851e7929f618a
VPAVANI30 <155209362+VPAVANI30@users.noreply.github.com>
2025-06-19 23:53:35 +0530

    app.py

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def index():
     return render_template_string(html, result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
